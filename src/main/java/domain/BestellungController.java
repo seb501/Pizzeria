2f432096b55a5ba4287cdc239e92aeb7999de359
Sebastian Titsch <titsch@th-brandenburg.de>
2024-05-08 10:18:38 +0200

    added main and java folder with packages

---
+++ src/main/java/domain/BestellungController.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BestellungController {
+  private BestellungView view;
+
+  public BestellungController(BestellungView view) {
+    this.view = view;
+
+    // Attach action listener to the orderButton
+    view.getBestellenButton().addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        // Retrieve text from orderArea and append it to the orderArea
+        String newText = view.getOrderArea().getText() + "\n" + view.getOrderField().getText();
+        view.getOrderArea().setText(newText);
+        // Clear the text field after placing the order
+        view.getOrderField().setText("");
+      }
+    });
+
+  }
+
+}
